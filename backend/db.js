8731562b43403e158521337ca9a6be4b467f23b1
Aakash Maharjan <aakash0977@gmail.com>
2023-08-15 21:34:01 +0545

    Add user validation to handle errors

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  await mongoose.connect('mongodb://127.0.0.1:27017/inotebook');
+  console.log("Connected to db");
 }
 
 module.exports = main;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,25 @@
     const express = require('express');
     const router = express.Router();
     const User = require('../models/User');
+    const {body, validationResult} = require('express-validator')
 
     //Create a USer using: POST "/api/auth". Doesn't require auth
-    router.post('/', (req, res)=>{
-        const user = User(req.body)
-        user.save();
-        res.send(req.body);
+    router.post('/',[
+        body('name', 'Enter a valid name').isLength({min:3}),
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Enter a valid passowrd').isLength({min:5})
+    ], (req, res)=>{
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+          return res.status(400).json({result: result.array()});
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }).then(user => res.json(user))
+        .catch(err => {console.log(err)
+        res.json({result:'please enter unique value', message: err.message})});
     })
 
     module.exports = router;
