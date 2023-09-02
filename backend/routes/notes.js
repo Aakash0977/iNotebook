300a43258eb5565798ccc45a942496928f85bf5d
Aakash Maharjan <aakash0977@gmail.com>
2023-09-02 14:48:45 +0545

    added update news of specific user note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -42,4 +42,25 @@ router.post('/addnote', fetchuser, [
 
 });
 
+// Route: 2 - Add notes of logged in user: PUT  "api/notes/updatenote/:id". No login require
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    //create a new note object 
+    const newNote = {};
+    if (title){newNote.title = title}
+    if (description){newNote.description = description}
+    if (tag){newNote.tag = tag}
+
+    //find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){ return res.status(404).send("Not found") }
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed")
+    }
+
+    note = await Note.findByIdAndUpdate (req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+
+});
+
 module.exports = router;
